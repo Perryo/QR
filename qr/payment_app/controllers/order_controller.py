3b54ba3b59bab4a7852d2cf20866745d687b3557
James <Perryo@users.noreply.github.com>
2019-11-12 21:33:25 -0800

    Adding stripe

--- qr/payment_app/controllers/order_controller.py
+++ qr/payment_app/controllers/order_controller.py
@@ -1,21 +1,42 @@
-from django.http import HttpResponse
+import stripe
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render_to_response
 from django.template import loader, RequestContext
 from django.core import serializers
 import json
 from payment_app.models import Order
 
+stripe.api_key = 'sk_test_jk6W0zrSFSnM4AcsN4hp5vAH'
+
+def handle_charge(request):
+    # Token is created using Checkout or Elements!
+    # Get the payment token ID submitted by the form:
+    token = request.POST.get('stripeToken')
+    order_id = request.POST.get('orderId')
+    amount = request.POST.get('amount')
+
+    if token:
+        charge = stripe.Charge.create(
+            amount=amount,
+            currency='usd',
+            description='QR charge',
+            source=token,
+        )
+        if not charge.failure_code:
+            # Set order paid
+            order = Order.objects.get(order_id=order_id)
+            order.paid = True
+            order.save()
+            return build_order_template(order)
+
+    else:
+        return HttpResponseBadRequest()
+
 
 def handle_order(request, order_id):
-    # TODO: Look up the order
     order = Order.objects.get(order_id=order_id)
-    # TODO: Check if the order is paid
-    data = json.loads(serializers.serialize("json", [order]))[0]
-    # TODO: Send price data to view
-
-    # TODO: Return view
-    template = loader.get_template('order_template.html')
-    return HttpResponse(template.render(data))
+    # If order is already paid view will handle it
+    return build_order_template(order)
 
 
 def dashboard(request):
@@ -23,3 +44,9 @@ def dashboard(request):
     orders = Order.objects.all()
     data = {'orders': orders}
     return render_to_response('dashboard.html', data, context)
+
+
+def build_order_template(order):
+    data = json.loads(serializers.serialize("json", [order]))[0]
+    template = loader.get_template('order_template.html')
+    return HttpResponse(template.render(data))
--- qr/payment_app/urls.py
+++ qr/payment_app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('charge', views.charge, name='charge'),
     path('order/<int:order_id>', views.order, name='order'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
