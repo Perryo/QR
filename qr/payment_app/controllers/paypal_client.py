8e42c0ec4271e0a59d49746a0bc7d4b25acc65c3
James <Perryo@users.noreply.github.com>
2019-11-17 16:22:24 -0800

    Moving payment into backend

---
+++ qr/payment_app/controllers/paypal_client.py
@@ -0,0 +1,63 @@
+from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment
+# 1. Import the PayPal SDK client that was created in `Set up Server-Side SDK`.
+from paypalcheckoutsdk.orders import OrdersGetRequest
+
+import sys
+
+from payment_app import apps
+
+
+class PayPalClient:
+    def __init__(self):
+        self.client_id = apps.PAYPAL_CLIENT_ID
+        self.client_secret = apps.PAYPAL_CLIENT_SECRET
+
+        """Set up and return PayPal Python SDK environment with PayPal access credentials.
+           This sample uses SandboxEnvironment. In production, use LiveEnvironment."""
+
+        self.environment = SandboxEnvironment(client_id=self.client_id, client_secret=self.client_secret)
+
+        """ Returns PayPal HTTP client instance with environment that has access
+            credentials context. Use this instance to invoke PayPal APIs, provided the
+            credentials have access. """
+        self.client = PayPalHttpClient(self.environment)
+
+    def object_to_json(self, json_data):
+        """
+        Function to print all json data in an organized readable manner
+        """
+        result = {}
+        if sys.version_info[0] < 3:
+            itr = json_data.__dict__.iteritems()
+        else:
+            itr = json_data.__dict__.items()
+        for key,value in itr:
+            # Skip internal attributes.
+            if key.startswith("__"):
+                continue
+            result[key] = self.array_to_json_array(value) if isinstance(value, list) else\
+                        self.object_to_json(value) if not self.is_primittive(value) else\
+                         value
+        return result
+
+    def array_to_json_array(self, json_array):
+        result =[]
+        if isinstance(json_array, list):
+            for item in json_array:
+                result.append(self.object_to_json(item) if  not self.is_primittive(item) \
+                              else self.array_to_json_array(item) if isinstance(item, list) else item)
+        return result
+
+    def is_primittive(self, data):
+        return isinstance(data, str) or isinstance(data, unicode) or isinstance(data, int)
+
+
+class GetOrder(PayPalClient):
+    # 2. Set up your server to receive a call from the client
+    def get_order(self, order_id):
+        # Method to get order
+        request = OrdersGetRequest(order_id)
+        # 3. Call PayPal to get the transaction
+        response = self.client.execute(request)
+
+        return response
